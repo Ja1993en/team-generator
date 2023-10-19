2a15ba59dd8ba86fb5b68a4120231210d507d078
Basquiat <jlnmcneal@gmail.com>
2023-10-19 00:12:19 -0500

    render object input to HTML

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import inquirer from "inquirer";
 import { Intern } from "./lib/intern.js";
 import { Engineer} from './lib/engineer.js'
 import { Manager } from './lib/manager.js';
+import * as fs from 'fs'
 
 //This collects all of the data 
 const teamArr = [];
@@ -49,7 +50,7 @@ const managerInfo =
 
 
     // This is a seperate veriable for add member so it can be called 
-    // independently and call the next step based on user input 
+    // independently then call the next step based on user input 
 const addEmployee = [
     {
         type: 'list',
@@ -117,7 +118,7 @@ const internInfo = [
 // This is the function that ignitiate the app. The will only be one 
 // 
 const managerInput = async () =>{
-    // try /catch is a error handling method it will try the 
+    // try /catch is a error handling method it will try the function
     // it will follow the instructions until the code is broken and return a error
     try{
         // This sets a variable and use async await to called the inquirer object 
@@ -126,7 +127,8 @@ const managerInput = async () =>{
         const ManagerObj = new Manager(ManagerRes.name, ManagerRes.id, ManagerRes.email, ManagerRes.office)
         //We are storing the new Manager object to a global variable 
         // empty global object to collect all inquirer responses 
-        teamArr.push(ManagerObj);
+        teamArr.push(ManagerObj.getHTML());
+        
         console.log(teamArr)
         // Calls the add Member function
         addMember();
@@ -154,35 +156,76 @@ const managerInput = async () =>{
                 const internRes = await inquirer.prompt(internInfo)
                 const employeeObj = new Intern(newEmployeeRes.name, newEmployeeRes.id, newEmployeeRes.email, internRes.school)
                 
-                teamArr.push(employeeObj);
+                teamArr.push(employeeObj.getHTML());
                 console.log(teamArr)
             }else {
                 const newEmployeeRes = await inquirer.prompt(employeeInfo)
                 const engineerRes = await inquirer.prompt(engineerInfo)
                 const engineerObj = new Engineer(newEmployeeRes.name, newEmployeeRes.id, newEmployeeRes.email, engineerRes.github)
-                teamArr.push(engineerObj);
+                // getHTML is calling the function from the class object 
+                teamArr.push(engineerObj.getHTML());
                 console.log(teamArr)
 
             }
-
             // this add member function is at the bottom of the original 
-            //iif statement because after the function is done collected data 
-            //it runs the function again to check if you would like to add another
-            // This is why I had to seperated manager function so it will not as to add another manager 
+            //if statement comes after the function is done collected data 
+            //it runs the function again to check if you would like to add another member
+            // This is why I had to seperated manager function so it will not add another manager 
             addMember();
         }else if(role === 'I dont want to add another team member'){
+
             console.log('Your team is complete')
+            //Place the if statement inside of the else if because
+            //Once your team is complete you want to render the HTML page
+
+            htmlPage();
         }
      
-        
+
     }
 
-    // Have a function that will loop through object and input the data that is necessary 
-
-//We need to gather all data from global object and fs write file 
 
 
-managerInput();
 
 
-// init();
+const htmlPage = async () =>{
+   
+    fs.writeFile('index.html', `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="./assets/main.css">
+        <title>Document</title>
+    </head>
+    <body>
+    
+    
+    
+    <header class="jumbotron">
+        <h1>Team Builder</h1>
+    </header>
+    
+    <div class=" member d-flex align-items-end justify-content-center flex-row bd-highlight mb-3">
+    ${teamArr}
+    </div>
+    
+    </div>
+    
+    
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+        <script src="./index.js"></script>
+    </body>
+    </html>
+    `
+     , (err) => {
+    if (err) throw err;
+    console.log('Team Saved!');
+});
+}
+
+ managerInput();
